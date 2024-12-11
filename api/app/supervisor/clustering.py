2181f94c4631cae63daaca9c243d52d927233239
Thinh Vo <vocuongthinh33@gmail.com>
2024-12-12 03:03:44 +0700

    init project

--- api/app/supervisor/clustering.py
+++ api/app/supervisor/clustering.py
@@ -11,37 +11,39 @@ from sklearn.preprocessing import normalize
 import pandas as pd
 from sklearn.decomposition import PCA
 import hdbscan
-for i in range(1, 1000):
-    mmn = must_and_must_not_query(params={"user_id": i})
-    result = es.raw_search_query(start=1, size=10000, must=mmn.get('must', []), must_not=mmn.get('must_not', []), should=mmn.get('should', []), aggs={})
-    list_id = []
-    list_embb = []
-    if result.get('data') == []:
-        continue
-    for data in result.get('data'):
-        for item in data.get("face_embedding"):
-            list_id.append(data.get('_id'))
-            list_embb.append(item.get('embedding'))
-    df = pd.DataFrame({'list_id': list_id, 'embedding': list_embb})
-    df = df[[ 'list_id',  'embedding']]
-    embeddings = np.vstack(df['embedding'].to_numpy())
-    ids = df['list_id'].to_list()
+while True:
+    try:
+        for i in range(1, 1000):
+            mmn = must_and_must_not_query(params={"user_id": i})
+            result = es.raw_search_query(start=1, size=10000, must=mmn.get('must', []), must_not=mmn.get('must_not', []), should=mmn.get('should', []), aggs={})
+            list_id = []
+            list_embb = []
+            if result.get('data') == []:
+                continue
+            for data in result.get('data'):
+                for item in data.get("face_embedding"):
+                    list_id.append(data.get('_id'))
+                    list_embb.append(item.get('embedding'))
+            df = pd.DataFrame({'list_id': list_id, 'embedding': list_embb})
+            df = df[[ 'list_id',  'embedding']]
+            embeddings = np.vstack(df['embedding'].to_numpy())
+            ids = df['list_id'].to_list()
 
-#    pca = PCA(n_components=50)
-#    reduced_embeddings = pca.fit_transform(embeddings)
+        #    pca = PCA(n_components=50)
+        #    reduced_embeddings = pca.fit_transform(embeddings)
 
-    hdbscan_clusterer = hdbscan.HDBSCAN(min_cluster_size=3)
-    labels = hdbscan_clusterer.fit_predict(embeddings)
-    df['cluster'] = labels
-    for id, cluster, emmb in zip(ids, labels, embeddings):
-        img = es.get_by_id(id)
-        face_embedding = img.get('face_embedding')
-        for item in face_embedding:
-            if item.get('embedding')[0] == emmb[0]:
-                item['cluster'] = cluster
-        es.update(id, {"face_embedding": img.get('face_embedding')})
-
-
-
-import time
-time.sleep(10)
+            hdbscan_clusterer = hdbscan.HDBSCAN(min_cluster_size=3)
+            labels = hdbscan_clusterer.fit_predict(embeddings)
+            df['cluster'] = labels
+            for id, cluster, emmb in zip(ids, labels, embeddings):
+                img = es.get_by_id(id)
+                face_embedding = img.get('face_embedding')
+                for item in face_embedding:
+                    if item.get('embedding')[0] == emmb[0]:
+                        item['cluster'] = cluster
+                es.update(id, {"face_embedding": img.get('face_embedding')})
+        import time
+        time.sleep(10)
+    except Exception as e:
+        print(e)
+        time.sleep(10)
