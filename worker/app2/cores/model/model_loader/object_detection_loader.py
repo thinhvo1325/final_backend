b0eb823174df7470ffb51e97bf7a6bbbd3022176
Thinh Vo <vocuongthinh33@gmail.com>
2024-12-12 02:14:17 +0700

    init project

--- worker/app2/cores/model/model_loader/object_detection_loader.py
+++ worker/app2/cores/model/model_loader/object_detection_loader.py
@@ -2,37 +2,141 @@ import tensorflow as tf
 import numpy as np
 from six import BytesIO
 from PIL import Image
-tf.keras.backend.clear_session()
+from transformers import AutoImageProcessor, DeformableDetrForObjectDetection
+import torch
+id_use = [2,10,
+11,
+18,
+22,
+28,
+33,
+34,
+ 35,
+ 44,
+ 56,
+ 60,
+ 76,
+ 79,
+ 87,
+ 93,
+ 98,
+ 109,
+ 117,
+ 126,
+ 131,
+ 132,
+ 138,
+ 142,
+ 145,
+ 149,
+ 159,
+ 172,
+ 176,
+ 182,
+ 188,
+ 193,
+ 206,
+ 216,
+ 224,
+ 231,
+ 270,
+ 276,
+ 295,
+ 343,
+ 360,
+ 377,
+ 391,
+ 408,
+ 410,
+ 428,
+ 460,
+ 468,
+ 497,
+ 499,
+ 543,
+ 588,
+ 591,
+ 614,
+ 616,
+ 625,
+ 630,
+ 641,
+ 644,
+ 654,
+ 674,
+ 686,
+ 693,
+ 702,
+ 704,
+ 714,
+ 734,
+ 747,
+ 792,
+ 799,
+ 810,
+ 815,
+ 817,
+ 827,
+ 834,
+ 836,
+ 875,
+ 880,
+ 884,
+ 897,
+ 910,
+ 911,
+ 920,
+ 922,
+ 942,
+ 946,
+ 947,
+ 950,
+ 958,
+ 961,
+ 980,
+ 981,
+ 999,
+ 1007,
+ 1020,
+ 1025,
+ 1026,
+ 1034,
+ 1036,
+ 1041,
+ 1049,
+ 1070,
+ 1071,
+ 1077,
+ 1078,
+ 1096,
+ 1098,
+ 1101,
+ 1109,
+ 1111,
+ 1114,
+ 1116,
+ 1121,
+ 1122,
+ 1132,
+ 1140,
+ 1160,
+ 1161]
 class ObjectDetection():
     def __init__(self):
         self.predicter = tf.saved_model.load("cores/model/model/saved_model")
-        
+        self.processor = AutoImageProcessor.from_pretrained("facebook/deformable-detr-box-supervised")
+        self.model = DeformableDetrForObjectDetection.from_pretrained("facebook/deformable-detr-box-supervised")
+
     def predict(self, file_path):
-        # result = self.predicter.recognize([file_path])
-        # return result[0]
-        image_np = self.load_image_into_numpy_array(file_path)
-        output_dict = self.run_inference_for_single_image(image_np)
+        image = Image.open(file_path)
+        inputs = self.processor(images=image, return_tensors="pt")
+        outputs = self.model(**inputs)
+        target_sizes = torch.tensor([image.size[::-1]])
+        results = self.processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.6)[0]
+        output_dict = []
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+            if label.item() not in id_use:
+                continue
+            box = [round(i, 2) for i in box.tolist()]
+            output_dict.append({"box": str(list(box)), "class": int(label.item()), "score": str(round(score.item(), 3))})
         return output_dict
-    
-    def run_inference_for_single_image(self, image):
-  
-        image = np.asarray(image)
-        input_tensor = tf.convert_to_tensor(image)
-        input_tensor = input_tensor[tf.newaxis,...]
-
-        model_fn = self.predicter.signatures['serving_default']
-        output_dict = model_fn(input_tensor)
-
-        num_detections = int(output_dict.pop('num_detections'))
-        output_dict = {key:value[0, :num_detections].numpy() 
-                    for key,value in output_dict.items()}
-        output_dict['num_detections'] = num_detections
-        output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-        return output_dict
-
-    def load_image_into_numpy_array(self, path):
-        img_data = tf.io.gfile.GFile(path, 'rb').read()
-        image = Image.open(BytesIO(img_data))
-        (im_width, im_height) = image.size
-        return np.array(image.getdata()).reshape(
-        (im_height, im_width, 3)).astype(np.uint8)
--- worker/app2/cores/model/predict_task.py
+++ worker/app2/cores/model/predict_task.py
@@ -22,27 +22,20 @@ class PredictTask(object):
     
     def draw_image(self, file_path, list_object):
         image = Image.open(file_path)
-        image_width, image_height = image.size 
         draw = ImageDraw.Draw(image)
         for obj in list_object:
             box = ast.literal_eval(obj["box"])
-            y_min, x_min, y_max, x_max = box
-            x_min_pixel = int(x_min * image_width)
-            x_max_pixel = int(x_max * image_width)
-            y_min_pixel = int(y_min * image_height)
-            y_max_pixel = int(y_max * image_height)
-            draw.rectangle([x_min_pixel, y_min_pixel, x_max_pixel, y_max_pixel], outline="red", width=1)
+            x_min, y_min, x_max, y_max = box
+            draw.rectangle([x_min, y_min, x_max, y_max], outline="red", width=1)
             label = f"Class {obj['class']}, {float(obj['score']):.2f}%"
-            draw.text((x_min_pixel, y_min_pixel - 10), label, fill="red")
+            draw.text((x_min, y_min - 10), label, fill="red")
         image.save(file_path.replace(config('FOLDER_UPLOAD'),config('FOLDER_OBJECT')))
+
 
     def predict(self, task_id, file_path):
         try:
             data = self.model.predict(file_path)
-            object_detection_result = []
-            for box, classes, score in zip(data['detection_boxes'], data['detection_classes'], data['detection_scores']):
-                if score > 0.7:
-                    object_detection_result.append({'box': str(list(box)), 'class': int(classes), 'score': str(round(score,3)*100)})
+            object_detection_result = data
             status = json.loads(redis_connecter.get(task_id))
             status.update({'object_detection': object_detection_result})
             # redis_connecter.set(task_id, json.dumps(status))
